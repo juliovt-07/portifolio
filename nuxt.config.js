25f2bd19847932c2875e40b4289382c978a75689
juliovt-07 <juliocesaar.ma@gmail.com>
2021-12-14 21:46:32 -0300

    fix: Melhorar SEO do HEAD

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,42 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Olá, sou Júlio, um Desenvolvedor aventureiro no mundo UI/UX, e entusiasmado por criar o mais agradável aos olhos e consoles.' },
+      { property: "og:site_name", content: "SOU O JULIO" },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://julioeu.vercel.app",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Júlio - Portifólio",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "Portifólio do Desenvolvedor Web Júlio César.",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "/logo.png",
+      },
+      { hid: 'description', name: 'description', content: 'Portifólio do Desenvolvedor Web Júlio César.' },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'author', content: 'Julio Cesar' },
+      { name: 'copyright', content: '© 2021 Julio Cesar' },
+      { name: 'rating', content: 'general' },
       { name: 'keywords', content: 'front, end, julio, julio cesar, desenvolvedor, ui/ux, ui, ux, site, ladingpage, sites, web, design, web design' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        hid: "canonical",
+        rel: "canonical",
+        href: "https://julioeu.vercel.app",
+      },
     ]
   },
 
