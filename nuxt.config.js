b7af4f29e5e7294a2bc623b052c58eacc0f93a18
juliovt-07 <juliocesaar.ma@gmail.com>
2021-12-18 21:08:08 -0300

    feat: Configurar pixel facebook

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,25 @@ export default {
     htmlAttrs: {
       lang: 'pt-br'
     },
+    __dangerouslyDisableSanitizers: ['script'],
+    script: [
+      {
+        innerHTML: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '885533725356971');
+          fbq('track', 'PageView');
+        `,
+        type: 'text/javascript',
+        charset: 'utf-8'
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
